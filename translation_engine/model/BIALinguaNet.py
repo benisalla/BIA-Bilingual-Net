90816a421f5daf0ad92ed12e2923ae883e9289b1
benisalla <ismailbenalla52@gmail.com>
2024-08-15 12:52:56 +0100

    fixed a very funny issue

--- translation_engine/model/BIALinguaNet.py
+++ translation_engine/model/BIALinguaNet.py
@@ -174,107 +174,106 @@ class BIALinguaNet(nn.Module):
         self = self.to(device)
         self.eval()
 
-        with torch.no_grad():
-            k = beam_size  # Beam size
-            n_hypo = min(k, 10)  # n of hypotheses
-            vs = self.dv_size  # Vocab size
+        k = beam_size  # Beam size
+        n_hypo = min(k, 10)  # n of hypotheses
+        vs = self.dv_size  # Vocab size
 
-            if isinstance(sx, str):
-                ex = tokenizer.encode(sx)
-                ex = torch.LongTensor(ex).unsqueeze(0)
-            else:
-                ex = sx
-            ex = ex.to(device)
-            e_seq_len = torch.LongTensor([ex.size(1)]).to(device)
+        if isinstance(sx, str):
+            ex = tokenizer.encode(sx)
+            ex = torch.LongTensor(ex).unsqueeze(0)
+        else:
+            ex = sx
+        ex = ex.to(device)
+        e_seq_len = torch.LongTensor([ex.size(1)]).to(device)
 
-            # Encoder Forward
-            ex = self.encoder(idx=ex, seq_len=e_seq_len)
+        # Encoder Forward
+        ex = self.encoder(idx=ex, seq_len=e_seq_len)
 
-            hypo = torch.LongTensor([[tokenizer.bos_token_id]]).to(
+        hypo = torch.LongTensor([[tokenizer.bos_token_id]]).to(
+            device
+        )  # d_idx: <SOS>
+        hypo_len = torch.LongTensor([hypo.size(1)]).to(device)  # d_seq_len: 1
+        hypo_scores = torch.zeros(1).to(device)  # 1 score
+
+        com_hypo = list()
+        com_hypo_scores = list()
+
+        step = 1
+        while True:
+            s = hypo.size(0)  # s
+            logits = self.decoder(
+                d_idx=hypo,
+                d_seq_len=hypo_len,
+                e_x=ex.repeat(s, 1, 1),
+                e_seq_len=e_seq_len.repeat(s),
+            )  # [s, step, vs]
+            flogits = self.top_k_top_p_filtering(
+                logits=logits[:, -1, :], top_k=top_k, top_p=top_p
+            )
+            scores = flogits / max(temperature + 1.0, 1e-8)  # [s, vs]
+            scores = F.log_softmax(scores, dim=-1)      # [s, vs]
+            scores = hypo_scores.unsqueeze(1) + scores  # prev scores + curr scores
+
+            top_k_hypo_scores, fttn_idx = scores.view(-1).topk(
+                k, 0, True, True
+            )  # top(vs) = k
+
+            prev_tok_idx = fttn_idx // vs  # prev [k]
+            next_tok_idx = fttn_idx % vs   # next [k]
+
+            top_k_hypo = torch.cat(
+                [hypo[prev_tok_idx], next_tok_idx.unsqueeze(1)], dim=1
+            )  # [k, step + 1]
+
+            complete = next_tok_idx == tokenizer.eos_token_id  # <EOS>? : [k], bool
+
+            com_hypo.extend(top_k_hypo[complete].tolist())
+            norm = math.pow(
+                ((5 + step) / (5 + 1)), len_norm_coeff
+            )  # chance(long sentence ~ short sentence)
+            com_hypo_scores.extend((top_k_hypo_scores[complete] / norm).tolist())
+
+            if len(com_hypo) >= n_hypo:
+                break  # enough hypos
+
+            hypo = top_k_hypo[~complete]  # [s, step + 1] incomplete hypos
+            hypo_scores = top_k_hypo_scores[~complete]  # d_idx(s): [s]
+            hypo_len = torch.LongTensor(hypo.size(0) * [hypo.size(1)]).to(
                 device
-            )  # d_idx: <SOS>
-            hypo_len = torch.LongTensor([hypo.size(1)]).to(device)  # d_seq_len: 1
-            hypo_scores = torch.zeros(1).to(device)  # 1 score
+            )  # d_seq_len: [s]
 
-            com_hypo = list()
-            com_hypo_scores = list()
+            if step > max_beam_fork:
+                break  # stop if no EOS is found
+            step += 1
 
-            step = 1
-            while True:
-                s = hypo.size(0)  # s
-                logits = self.decoder(
-                    d_idx=hypo,
-                    d_seq_len=hypo_len,
-                    e_x=ex.repeat(s, 1, 1),
-                    e_seq_len=e_seq_len.repeat(s),
-                )  # [s, step, vs]
-                flogits = self.top_k_top_p_filtering(
-                    logits=logits[:, -1, :], top_k=top_k, top_p=top_p
-                )
-                scores = flogits / max(temperature + 1.0, 1e-8)  # [s, vs]
-                scores = F.log_softmax(scores, dim=-1)      # [s, vs]
-                scores = hypo_scores.unsqueeze(1) + scores  # prev scores + curr scores
+        if len(com_hypo) == 0:  # in case no EOS is found
+            com_hypo = hypo.tolist()
+            com_hypo_scores = hypo_scores.tolist()
 
-                top_k_hypo_scores, fttn_idx = scores.view(-1).topk(
-                    k, 0, True, True
-                )  # top(vs) = k
-
-                prev_tok_idx = fttn_idx // vs  # prev [k]
-                next_tok_idx = fttn_idx % vs   # next [k]
-
-                top_k_hypo = torch.cat(
-                    [hypo[prev_tok_idx], next_tok_idx.unsqueeze(1)], dim=1
-                )  # [k, step + 1]
-
-                complete = next_tok_idx == tokenizer.eos_token_id  # <EOS>? : [k], bool
-
-                com_hypo.extend(top_k_hypo[complete].tolist())
-                norm = math.pow(
-                    ((5 + step) / (5 + 1)), len_norm_coeff
-                )  # chance(long sentence ~ short sentence)
-                com_hypo_scores.extend((top_k_hypo_scores[complete] / norm).tolist())
-
-                if len(com_hypo) >= n_hypo:
-                    break  # enough hypos
-
-                hypo = top_k_hypo[~complete]  # [s, step + 1] incomplete hypos
-                hypo_scores = top_k_hypo_scores[~complete]  # d_idx(s): [s]
-                hypo_len = torch.LongTensor(hypo.size(0) * [hypo.size(1)]).to(
-                    device
-                )  # d_seq_len: [s]
-
-                if step > max_beam_fork:
-                    break  # stop if no EOS is found
-                step += 1
-
-            if len(com_hypo) == 0:  # in case no EOS is found
-                com_hypo = hypo.tolist()
-                com_hypo_scores = hypo_scores.tolist()
-
-            all_hypos = list()  # idx ==> string
-            spec_toks = [
-                tokenizer.pad_token_id,
-                tokenizer.bos_token_id,
-                tokenizer.eos_token_id,
+        all_hypos = list()  # idx ==> string
+        spec_toks = [
+            tokenizer.pad_token_id,
+            tokenizer.bos_token_id,
+            tokenizer.eos_token_id,
+        ]
+        if is_ltr:
+            com_hypo = [
+                tokenizer.decode([t for t in s if t not in spec_toks])[::-1]
+                for s in com_hypo
             ]
-            if is_ltr:
-                com_hypo = [
-                    tokenizer.decode([t for t in s if t not in spec_toks])[::-1]
-                    for s in com_hypo
-                ]
-            else:
-                com_hypo = [
-                    tokenizer.decode([t for t in s if t not in spec_toks])
-                    for s in com_hypo
-                ]
+        else:
+            com_hypo = [
+                tokenizer.decode([t for t in s if t not in spec_toks])
+                for s in com_hypo
+            ]
 
-            for i, h in enumerate(com_hypo):
-                all_hypos.append({"hypothesis": h, "score": com_hypo_scores[i]})
+        for i, h in enumerate(com_hypo):
+            all_hypos.append({"hypothesis": h, "score": com_hypo_scores[i]})
 
-            max_idx = com_hypo_scores.index(max(com_hypo_scores))
-            best_hypo = all_hypos[max_idx]["hypothesis"]
+        max_idx = com_hypo_scores.index(max(com_hypo_scores))
+        best_hypo = all_hypos[max_idx]["hypothesis"]
 
-            return best_hypo, all_hypos
+        return best_hypo, all_hypos
 
     def top_k_top_p_filtering(
         self,
@@ -285,8 +284,7 @@ class BIALinguaNet(nn.Module):
         min_tokens_to_keep: int = 1,
     ) -> torch.Tensor:
         if top_k > 0:
-            top_k = min(max(top_k, min_tokens_to_keep), logits.size(-1))  # Safety check
-            # Remove all tokens with a probability less than the last token of the top-k
+            top_k = min(max(top_k, min_tokens_to_keep), logits.size(-1)) 
             indices_to_remove = logits < torch.topk(logits, top_k)[0][..., -1, None]
             logits[indices_to_remove] = filter_value
 
@@ -294,18 +292,14 @@ class BIALinguaNet(nn.Module):
             sorted_logits, sorted_indices = torch.sort(logits, descending=True)
             cumulative_probs = torch.cumsum(F.softmax(sorted_logits, dim=-1), dim=-1)
 
-            # Remove tokens with cumulative probability above the threshold (token with 0 are kept)
             sorted_indices_to_remove = cumulative_probs > top_p
             if min_tokens_to_keep > 1:
-                # Keep at least min_tokens_to_keep (set to min_tokens_to_keep-1 because we add the first one below)
                 sorted_indices_to_remove[..., :min_tokens_to_keep] = 0
-            # Shift the indices to the right to keep also the first token above the threshold
             sorted_indices_to_remove[..., 1:] = sorted_indices_to_remove[
                 ..., :-1
             ].clone()
             sorted_indices_to_remove[..., 0] = 0
 
-            # scatter sorted tensors to original indexing
             indices_to_remove = sorted_indices_to_remove.scatter(
                 1, sorted_indices, sorted_indices_to_remove
             )
